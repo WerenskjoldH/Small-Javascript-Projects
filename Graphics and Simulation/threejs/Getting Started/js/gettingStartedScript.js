22229e490f5d2c2a779fa58743345c719a6bce96
Hunter Werenskjold <hunter@skeletoncrew.co.jp>
2020-10-09 10:15:01 +0900

    Update gettingStartedScript.js

--- Graphics and Simulation/threejs/Getting Started/js/gettingStartedScript.js
+++ Graphics and Simulation/threejs/Getting Started/js/gettingStartedScript.js
@@ -9,12 +9,24 @@ document.body.appendChild(renderer.domElement);
 
 // Define shape
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: false});
+var materialTwo = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: false});
 var cube = new THREE.Mesh(geometry, material);
+var cubeTwo = new THREE.Mesh(geometry, materialTwo);
 scene.add(cube);
+scene.add(cubeTwo);
+
+camera.position.z = 3;
+
+const STEPSIZE = 0.01;
+var dt = 0;
 
 var Update = function () {
+    cube.position.y = 1.5 * Math.sin(dt);
+    cube.position.x = 1.5 * Math.cos(dt);
 
+
+    dt += STEPSIZE;
 };
 
 var Render = function () {
@@ -30,6 +42,8 @@ var GameLoop = function () {
 }
 
 GameLoop();
+
+// Other
 
 window.addEventListener('resize', function() {
     renderer.setSize(window.innerWidth, window.innerHeight);
